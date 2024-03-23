98e22e201731d092bdf5eb039e5031c857621f77
SanSayArt <SanSayArt@gmail.com>
2024-03-23 13:39:23 +0500

    All_1

--- src/control/Control.java
+++ src/control/Control.java
@@ -10,9 +10,8 @@ public class Control {
     private Basket<Fruit> basket;
     private static String menuPoint = " 1 - Вывести содержимоле корзины %n" +
             "2 - Вывести список всего фруктов в корзине %n"+
-            "3 - Вывести самое длинное название фрукта в корзине %n"+
-            "4 - Вывести самые популярные фрукты в корзине %n"+
-            "5 - Вывести самые не популярные фрукты в корзине %n"+
+            "3 - Найти самое длинное название фрукта в корзине %n"+
+            "4 - Вывести фрукты в корзине в порядке уменьшения количества %n"+
             "0 - Выйти из приложения %n"+
             "Введите номер операции: ";
     private GetSelect getSelect;
@@ -30,8 +29,7 @@ public class Control {
                 case 1 -> new CalcAllFruit();
                 case 2 -> new CalcCountFruit();
                 case 3 -> new CalcLongName();
-                case 4 -> new CalcFruitRate(false);
-                case 5 -> new CalcFruitRate(true);
+                case 4 -> new CalcFruitRate();
                 default -> null;
             };
             if (calc!=null) {
--- src/view/CalcFruitRate.java
+++ src/view/CalcFruitRate.java
@@ -11,12 +11,8 @@ import static java.util.Map.Entry.comparingByValue;
 import static java.util.stream.Collectors.toMap;
 
 public class CalcFruitRate implements Calc<Basket> {
-    private static boolean ascending = false;
-
     public CalcFruitRate() {
     }
-
-    public CalcFruitRate(boolean ascending) {CalcFruitRate.ascending = ascending;}
 
     @Override
     public void calc(Basket basket) {
@@ -30,23 +26,11 @@ public class CalcFruitRate implements Calc<Basket> {
         Map<Fruit, Integer> sorted = basket.getBasket();
 
         Object LinkedHashMap;
-        if (!ascending) {
-            sorted = sorted
-                    .entrySet()
-                    .stream()
-                    .sorted(Collections.reverseOrder(comparingByValue()))
-                    .collect(
-                            toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
-        } else {
-            sorted = sorted
-                    .entrySet()
-                    .stream()
-                    .sorted(comparingByValue())
-                    .collect(
-                            toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
-
-        }
+        sorted = sorted
+                .entrySet()
+                .stream()
+                .sorted(Collections.reverseOrder(comparingByValue()))
+                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
         return sorted;
     }
-    public static void setAscending(boolean ascending){ CalcFruitRate.ascending = ascending;}
 }
